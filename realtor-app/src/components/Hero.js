e540abe1935ebcb95607a00241364bf9456f2dc3
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-08-07 13:02:24 -0400

    Created initial login and added to the hero.js file. Added corresponding styling in the hero.css file.

--- realtor-app/src/components/Hero.js
+++ realtor-app/src/components/Hero.js
@@ -6,10 +6,22 @@ import SearchForm from '../components/form';
 
 function Hero() {
   return (
-    <div className="hero hero-lg mb-3">
-      <div className="hero-body text-center text-light">
-        <h1>There's No Place Like Home</h1>
-        <SearchForm/>
+    <div class="container">
+      <div class="login">
+        <div class="tab">
+          <button class="tablinks" onclick="">Login</button>
+          <button class="tablinks" onclick="">Register</button>
+        </div>
+        <div class="signin">
+          <div class="inputs">
+            <p>Username</p>
+            <input id="username" type="text" />
+            <p>Password</p>
+            <input id="password" type="text" />
+          </div>
+          <button id="submit" type="submit">Submit</button>
+          <a href="" id="recover">Forgot your password?</a>
+        </div>
       </div>
     </div>
   )
