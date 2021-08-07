afa189e128b53bbc4306fcd2665a32217196af18
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-08-07 14:01:43 -0400

    Created Register form and styled.

--- realtor-app/src/components/Hero.js
+++ realtor-app/src/components/Hero.js
@@ -24,10 +24,11 @@ function Hero() {
           <a href="" id="recover">Forgot your password?</a>
         </div>
 
-    <div className="hero hero-lg mb-3">
-      <div className="hero-body text-center text-light">
-        <h1>There's No Place Like Home</h1>
-
+        <div className="hero hero-lg mb-3">
+          <div className="hero-body text-center text-light">
+            <h1>There's No Place Like Home</h1>
+          </div>
+        </div>
       </div>
     </div>
   )
