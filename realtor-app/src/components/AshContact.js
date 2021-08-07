d2aedeb78bc105695997dc38789d92f25b143242
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-08-07 17:22:16 -0400

    Added personal info to contact.

--- realtor-app/src/components/AshContact.js
+++ realtor-app/src/components/AshContact.js
@@ -1,34 +1,35 @@
 import React from "react";
-
+import AshleighPic from '../images/AshleighPic.png';
 
 const AshContact = () => {
   return (
-   <div className="text-center">
-          <h2>Contact Information</h2>
-          <hr class="solid bg-dark" />
-          <img
-            style={{ height: "250px" }}
-            src=""
-            class=""
-            alt="picture"
-          />
-          <h4>
-            bio
-          </h4>
-          <hr className="" />
-          <h3>Cell Phone</h3>
-          <hr className="hr-dark w-25" />
-          <a href="tel:609-289-6244">
-            <div icon="phone-alt" /> 609-289-6244
+    <div className="text-center">
+      <h2>Contact Information</h2>
+      <hr class="solid bg-dark" />
+      <img
+        style={{ height: "250px" }}
+        src={AshleighPic}
+        class="selfie"
+        alt="picture"
+      />
+      <p>
+        <span class="first-letter">A</span>shleigh has made a career of always building. In the past she worked with zero energy and green friendly design firms to bring physical construction on par with the technologies and methods now available. She has shifted her focus, and now builds in a new way as a web developer. She brings her passion for solving problems and designing and creating new projects, from concept to reality.
+        </p>
+      <p>Currently located in Philadelphia, you can find Ashleigh living with her faithful blue nose pittie. She is driven by an insatiable curiousity that requires a constant stream of learning. When not working she is honing her skills, working out, cooking new dishes, and exploring both in the city and out.</p>
+      <hr className="" />
+      <h3>Cell Phone</h3>
+      <hr className="hr-dark w-25" />
+      <a href="tel:267-368-7744">
+        <div icon="phone-alt" /> 267-368-7744
           </a>
-          <hr className="" />
-          <h3>E-mail</h3>
-          <hr className="" />
-          <a href="mailto:batistaj651@gmail.com">
-            <div icon="envelope" /> batistaj651@gmail.com
+      <hr className="" />
+      <h3>E-mail</h3>
+      <hr className="" />
+      <a href="mailto:roseashtech@gmail.com">
+        <div icon="envelope" /> roseashtech@gmail.com
           </a>
-       </div>
-      
+    </div>
+
   );
 };
 
--- realtor-app/src/components/Navbar.js
+++ realtor-app/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import{menuData} from '../Data/MenuData';
+import { menuData } from '../Data/MenuData';
 import { Link } from 'react-router-dom';
-import styled, {css} from 'styled-components/macro';
+import styled, { css } from 'styled-components/macro';
 import { FaHome } from 'react-icons/fa';
 
 
@@ -16,7 +16,7 @@ z-index:100;
 background-color:rgb(111, 47, 47);
 width:100%;
 `
- const NavLink = css`
+const NavLink = css`
  color:#fff;
  display:flex;
  align-items:center;
@@ -31,33 +31,58 @@ ${NavLink}
 `
 const MenuBars = styled.i``;
 
-const NavMenu = styled.div `
+const NavMenu = styled.div`
 display:flex;
 justify-content: space-between;
 margin-right:0;
 `;
-const NavMenuLinks = styled(Link) `
+const NavMenuLinks = styled(Link)`
 ${NavLink} 
 font-size:20px;
 margin-right:15px`;
 
+// const Navbar = () => {
+//   return (
+
+//       <Nav>
+//       <Logo to="/">Home icon</Logo>
+//       <MenuBars/>
+//       <NavMenu>
+//         {menuData.map((item) => (
+//       <NavMenuLinks to={item.link} key={item.id}>
+//      {item.title}
+//       </NavMenuLinks>
+//         ))}
+//       </NavMenu>
+
+//       </Nav>
+
+
+//   )
+// }
+
 const Navbar = () => {
   return (
-    
-      <Nav>
-      <Logo to="/">Home icon</Logo>
-      <MenuBars/>
+
+    <Nav>
+      <Logo to="/"><FaHome size={75} /> </Logo>
+      <h1>There's No Place Like H<svg class="heart" viewBox="0 0 32 29.6">
+        <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
+  c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
+      </svg> me</h1>
+
+      <MenuBars />
       <NavMenu>
         {menuData.map((item) => (
-      <NavMenuLinks to={item.link} key={item.id}>
-     {item.title}
-      </NavMenuLinks>
+          <NavMenuLinks to={item.link} key={item.id}>
+            {item.title}
+          </NavMenuLinks>
         ))}
       </NavMenu>
-     
-      </Nav>
-      
-   
+
+    </Nav>
+
+
   )
 }
 
