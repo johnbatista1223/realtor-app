c8ec9c25cd13b686f6ac70965170d1ff5fbcfa3d
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-08-07 15:20:30 -0400

    Created the register.css file to style the RegisterUser.js componenet.

--- realtor-app/src/components/RegisterUser.js
+++ realtor-app/src/components/RegisterUser.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import '../styles/hero.css';
+import '../styles/register.css';
 
 const RegisterUser = () => {
 
     return (
-        <div class="register">
-            <div class="title">
-                <h1>Register with HomePage</h1>
-            </div>
-            <div class="info">
-                <div class="inputs">
-                    <p>Username</p>
-                    <input id="username" type="text" />
-                    <p>Email</p>
-                    <input id="email" type="text" />
-                    <p>Password</p>
-                    <input id="password" type="text" />
-                    <p>Confirm Password</p>
-                    <input id="confirm-password" type="text" />
+        <div className="container">
+            <div class="register">
+                <div class="title">
+                    <h1>Register with HomePage</h1>
                 </div>
-                <div class="reg-button">
-                    <button id="register-submit" type="submit">Submit</button>
+                <div class="info">
+                    <div class="inputs">
+                        <p>Username</p>
+                        <input id="username" type="text" />
+                        <p>Email</p>
+                        <input id="email" type="text" />
+                        <p>Password</p>
+                        <input id="password" type="text" />
+                        <p>Confirm Password</p>
+                        <input id="confirm-password" type="text" />
+                    </div>
+                    <div class="reg-button">
+                        <button id="register-submit" type="submit">Submit</button>
+                    </div>
                 </div>
             </div>
         </div>
