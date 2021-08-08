9ce67c9410bae08dff8cd32d2d0c3c1c6584c914
Amy Smerlick <77814900+amysmerlick@users.noreply.github.com>
2021-08-07 20:31:38 -0400

    deleted the footer.css file

--- realtor-app/src/components/Hero.js
+++ realtor-app/src/components/Hero.js
@@ -14,41 +14,32 @@ function Hero() {
           <h2>Login to HomePage</h2>
         </div>
       </div>
-      <div class='hero container'>
-        <div class="login">
-          <div class="tab">
-            <button class="tablinks" onclick="">Login</button>
-            <button class="tablinks" onclick="">Register</button>
-
-          </div>
-          <div class="signin">
-            <div class="inputs">
-              <p>Username</p>
-              <input
-                id="username"
-                type="text"
-                placeholder=" Type your username here..."
-              />
-              <p>Password</p>
-              <input
-                id="password"
-                type="text"
-                placeholder=" Type your password here..."
-              />
-            </div>
-            <a href="" id="recover">Forgot your password?</a>
-            <div id="login-button">
-              <button id="submit" type="submit">Submit</button>
-            </div>
-            <div id="register">
-              <h3>Or create an account</h3>
-              <button id="register-submit" type="submit">Register</button>
-            </div>
-          </div>
+      <div class="signin">
+        <div class="inputs">
+          <p>Username</p>
+          <input
+            id="username"
+            type="text"
+            placeholder=" Type your username here..."
+          />
+          <p>Password</p>
+          <input
+            id="password"
+            type="text"
+            placeholder=" Type your password here..."
+          />
+        </div>
+        <a href="" id="recover">Forgot your password?</a>
+        <div id="login-button">
+          <button id="submit" type="submit">Submit</button>
+        </div>
+        <div id="register">
+          <h3>Or create an account</h3>
+          <button id="register-submit" type="submit">Register</button>
         </div>
       </div>
     </div>
-      )
+  )
 }
 
-      export default Hero
+export default Hero
