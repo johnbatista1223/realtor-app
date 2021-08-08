47d5ed0d5322c72357eb3c0a91e380071c26526b
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-08-08 15:05:33 -0400

    Created contact components and styled accordingly.

--- realtor-app/src/components/AmyContact.js
+++ realtor-app/src/components/AmyContact.js
@@ -1,34 +1,44 @@
 import React from "react";
+import '../styles/contact-each.css';
 
+import Headshot from '../images/Headshot.jpg';
+import { FaGithub } from "react-icons/fa"
+import { FaLinkedin } from "react-icons/fa"
+import { FaPhone } from "react-icons/fa"
+import { FaEnvelope } from "react-icons/fa"
 
 const AmyContact = () => {
   return (
-   <div className="text-center">
-          <h2>Contact Information</h2>
-          <hr class="" />
-          <img
-            style={{ height: "250px" }}
-            src=""
-            class=""
-            alt="picture"
-          />
-          <h4>
-            bio
-          </h4>
-          <hr className="hr-light w-25" />
-          <h3>Cell Phone</h3>
-          <hr className="" />
-          <a href="tel:609-289-6244">
-            <div icon="phone-alt" /> 609-289-6244
-          </a>
-          <hr className="" />
-          <h3>E-mail</h3>
-          <hr className="" />
-          <a href="mailto:batistaj651@gmail.com">
-            <div icon="envelope" /> batistaj651@gmail.com
-          </a>
-       </div>
-      
+    <div className="text-center">
+      <div className="bio-pic">
+        <img
+          src={Headshot}
+          class="selfie"
+          alt="picture"
+        />
+        <div className="bio">
+          <p>
+            <span class="first-letter">Y</span>our text here
+        Currently located in Philadelphia, you can find Ashleigh living with her faithful blue nose pittie. She is driven by an insatiable curiousity that requires a constant stream of learning. When not working she is honing her skills, working out, cooking new dishes, and exploring both in the city and out.</p>
+          <div className="fa-icons">
+            <div className="contact-icon">
+              <a className="contact-icons" href="https://github.com/"><FaGithub size={30} /></a>
+            </div>
+            <div className="contact-icon">
+              <a className="contact-icons" href="https://linkedin.com/in/"><FaLinkedin size={30} /></a>
+            </div>
+            <div className="contact-icon">
+              <a className="contact-icons" href="+1"><FaPhone size={25} /></a>
+            </div>
+            <div className="contact-icon">
+              <a className="contact-icons" href="mailto:"><FaEnvelope size={30} /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
   );
 };
 
--- realtor-app/src/components/JohnContact.js
+++ realtor-app/src/components/JohnContact.js
@@ -1,38 +1,49 @@
 import React from "react";
+import '../styles/contact-each.css';
+
 import JohnsPic from '../images/john.jpg';
+import { FaGithub } from "react-icons/fa"
+import { FaLinkedin } from "react-icons/fa"
+import { FaPhone } from "react-icons/fa"
+import { FaEnvelope } from "react-icons/fa"
 
 
 
 const JohnContact = () => {
   return (
-   <div className="text-center">
-          <h2>Contact Information</h2>
-          <hr class="" />
-          <img
-            style={{ height: "200px" }}
-            src={JohnsPic}
-            class=""
-            alt="picture"
-          />
-          <h4>
-            hello my name is john batisa 
-          </h4>
-          <hr className="" />
-          <h3>Cell Phone</h3>
-          <hr className="" />
-          <a href="609-289-6244">
-            <div icon="phone-alt" /> 609-289-6244
-          </a>
-          <hr className="" />
-          <h3>E-mail</h3>
-          <hr className="" />
-          <a href="mailto:batistaj651@gmail.com">
-            <div icon="envelope" /> batistaj651@gmail.com
-          </a>
-       </div>
-         
-      
-      
+    <div className="text-center">
+      <div className="bio-pic">
+        <img
+          src={JohnsPic}
+          class="selfie"
+          alt="picture"
+        />
+        <div className="bio">
+          <p>
+            <span class="first-letter">Y</span>our text here
+        </p>
+          <p>Currently located in Philadelphia, you can find Ashleigh living with her faithful blue nose pittie. She is driven by an insatiable curiousity that requires a constant stream of learning. When not working she is honing her skills, working out, cooking new dishes, and exploring both in the city and out.</p>
+          <div className="fa-icons">
+            <div className="contact-icon">
+              <a className="contact-icons" href="https://github.com/"><FaGithub size={30} /></a>
+            </div>
+            <div className="contact-icon">
+              <a className="contact-icons" href="https://linkedin.com/in/"><FaLinkedin size={30} /></a>
+            </div>
+            <div className="contact-icon">
+              <a className="contact-icons" href="+1"><FaPhone size={25} /></a>
+            </div>
+            <div className="contact-icon">
+              <a className="contact-icons" href="mailto:"><FaEnvelope size={30} /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+
+
   );
 };
 
