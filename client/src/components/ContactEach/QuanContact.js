cff4dce91bb4758a4a2a3ef9e0ffdba4a6ae9c9d
Quando24 <stanley.raquan@gmail.com>
2021-08-11 19:05:39 -0400

    updating contact

--- client/src/components/ContactEach/QuanContact.js
+++ client/src/components/ContactEach/QuanContact.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './contact-each.css';
 
-import JohnsPic from '../images/john.jpg';
+import RaquanPic from '../images/RaquanPic.jpg';
 import { FaGithub } from "react-icons/fa"
 import { FaLinkedin } from "react-icons/fa"
 import { FaPhone } from "react-icons/fa"
@@ -13,27 +13,27 @@ const QuanContact = () => {
     <div className="text-center">
       <div className="bio-pic">
         <img
-          src={JohnsPic}
+          src={RaquanPic}
           class="selfie"
           alt="picture"
         />
         <div className="bio">
           <p>
-            <span class="first-letter">Y</span>our text here
+            <span class="first-letter">Y</span>Hey, my name is Raquan Stanley, and currently, I am a student representing the University of Pennsylvania's Full Stack Coding Bootcamp!
         </p>
-          <p>Currently located in Philadelphia, you can find Ashleigh living with her faithful blue nose pittie. She is driven by an insatiable curiousity that requires a constant stream of learning. When not working she is honing her skills, working out, cooking new dishes, and exploring both in the city and out.</p>
+          <p>Currently located in Allentown,PA ,you can find Raquan living with her faithful blue nose pittie. She is driven by an insatiable curiousity that requires a constant stream of learning. When not working she is honing her skills, working out, cooking new dishes, and exploring both in the city and out.</p>
           <div className="fa-icons">
             <div className="contact-icon">
-              <a className="contact-icons" href="https://github.com/"><FaGithub size={30} /></a>
+              <a className="contact-icons" href="https://https://github.com/Quando24"><FaGithub size={30} /></a>
             </div>
             <div className="contact-icon">
-              <a className="contact-icons" href="https://linkedin.com/in/"><FaLinkedin size={30} /></a>
+              <a className="contact-icons" href="https://www.linkedin.com/in/raquan-stanley/"><FaLinkedin size={30} /></a>
             </div>
             <div className="contact-icon">
               <a className="contact-icons" href="+1"><FaPhone size={25} /></a>
             </div>
             <div className="contact-icon">
-              <a className="contact-icons" href="mailto:"><FaEnvelope size={30} /></a>
+              <a className="contact-icons" href="mailto:stanley.raquan@gmail.com"><FaEnvelope size={30} /></a>
             </div>
           </div>
         </div>
