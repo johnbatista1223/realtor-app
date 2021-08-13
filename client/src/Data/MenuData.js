bd3fbe23bf9291021c5e5515a4041b615e4ac24e
Amy Smerlick <77814900+amysmerlick@users.noreply.github.com>
2021-08-12 21:52:47 -0400

    updated app.js file with toms corrections to error, and commented out a line in the HTML code that was causing a Heroku error

--- client/src/Data/MenuData.js
+++ client/src/Data/MenuData.js
@@ -1,11 +1,11 @@
-import { Carousel } from "bootstrap"
+// import { Carousel } from "bootstrap"
 
 export const menuData =[
   {title:"Homes", link:"/homes"},
   // {title:"Rentals", link:"/rentals"},
   {title:"Contact us", link:"/contact"},
   // {title:"Register", link:"/Register"},
-  {title:"Profile", link:"/Userprofile"},
+  {title:$username"Profile", link:"/Userprofile"},
   {title: 'Logout', link:"/logout"}
 
 ]
