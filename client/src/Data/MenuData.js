b95589d62a60d438c64f2f611bee6102c3c39d50
Amy Smerlick <77814900+amysmerlick@users.noreply.github.com>
2021-08-12 22:39:24 -0400

    updated API key

--- client/src/Data/MenuData.js
+++ client/src/Data/MenuData.js
@@ -1,11 +1,10 @@
-// import { Carousel } from "bootstrap"
 
 export const menuData =[
   {title:"Homes", link:"/homes"},
   // {title:"Rentals", link:"/rentals"},
   {title:"Contact us", link:"/contact"},
   // {title:"Register", link:"/Register"},
-  {title:$username"Profile", link:"/Userprofile"},
+  {title:"Profile", link:"/Userprofile"},
   {title: 'Logout', link:"/logout"}
 
 ]
