c7f6bf5eae86bb5550e7c6af09d9daa773c51346
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-08-14 10:10:14 -0400

    Changed footer structure.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,52 +1,29 @@
 import React from 'react';
 import '../styles/footer.css';
 
+
+
 //random note
 function Footer() {
 
     return (
-        <div class="footer">
-            <div class="contain">
-                <div class="col">
-                    <h1>Company</h1>
-                    <ul>
-                    <li><a href="/Contact">About</a></li>
-                    <li><a href="/Contact">Mission</a></li>
-                    <li><a href="/Contact">Services</a></li>
-                    <li><a href="/Contact">Social</a></li>
-                    <li><a href="/Contact">Contact</a></li>
-                    </ul>
+        <div className="footer">
+            <div className="footer-links">
+                <div className="page-links">
+                    <a className="footer-a after-line" href="/Homes">Homes</a>
+                    <a className="footer-a after-line" href="/Contact">Contact Us</a>
+                    <a className="footer-a after-line" href="">Profile</a>
+                    <a className="footer-a" href="">Logout</a>
                 </div>
-                <div class="col">
-                    <h1>Products</h1>
-                    <ul>                        
-                    <li><a href="/Homes">About</a></li>
-                    <li><a href="/Homes">Mission</a></li>
-                    <li><a href="/Homes">Services</a></li>
-                    <li><a href="/Homes">Social</a></li>
-                    <li><a href="/Homes">Contact</a></li>
-                    </ul>
+                <div className="coding-links">
+                    <a className="coding-a after-line" href="">Heroku</a>
+                    <a className="coding-a" href="">GitHub</a>
                 </div>
-                <div class="col">
-                    <h1>Accounts</h1>
-                    <ul>
-                    <li><a href="/Homes">About</a></li>
-                    <li><a href="/Homes">Mission</a></li>
-                    <li><a href="/Homes">Services</a></li>
-                    <li><a href="/Homes">Social</a></li>
-                    <li><a href="/Homes">Contact</a></li>
-                    </ul>
-                </div>
-                <div class="col social">
-                    <h1>Social</h1>
-                    <ul>
-                        <a href="https://github.com/johnbatista1223/realtor-app">
-                            <li class="list"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="black" className="bi bi-github" viewBox="0 0 16 16">
-                                <path class="github" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-                            </svg></li></a>
-                    </ul>
-                </div>
-                <div class="clearfix"></div>
+            </div>
+            <div className="info">
+                <p>Copyright © 2021 RAJA Realty & Development. All Rights Reserved.</p>
+                <p>405 Arnold Ave. Point Break, DE 08742 Phone: 732-475-9165</p>
+
             </div>
         </div>
 
@@ -54,30 +31,3 @@ function Footer() {
 }
 
 export default Footer
-
-{/* 
-                    // <div id="footer">
-        //     <div class="container">
-        //         <div class="row">
-        //             <div class="span2">
-        //                 <div class="footer-list">
-        //                     <a href="https://github.com/johnbatista1223/realtor-app">
-        //                         <li class="list">GitHub</li>
-        //                     </a>
-        //                     <a href="/Contact">
-        //                         <li class="list">Contact Us</li>
-        //                     </a>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        //     <div id="copyright-bar">
-        //         <div class="container">
-        //             <div class="row">
-        //                 <div class="copyright-content">Copyright 2021 All rights reserved | This site was made with by <a href="https://www.linkedin.com/in/ashleighfreiler/">Ashleigh Freilder</a><a href="https://www.linkedin.com/mwlite/in/john-Batista4">John Batista</a> <a href="https://www.linkedin.com/in/amysmerlick/">Amy Smerlick</a> <a href="https://www.linkedin.com/in/raquan-stanley/">Raquan Stanley</a>
-
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div> 
-        // </div> */}
