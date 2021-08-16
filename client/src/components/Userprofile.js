1e173702b644d16a2beb2f3859f844285f4bd160
Amy Smerlick <77814900+amysmerlick@users.noreply.github.com>
2021-08-16 17:37:45 -0400

    updated some css, divs need added to make cards and containers

--- client/src/components/Userprofile.js
+++ client/src/components/Userprofile.js
@@ -17,14 +17,14 @@ const Userprofile = () => {
   return (
     <div>
       {console.log("listings", data)}
-      <h1>hello</h1>
+      <h1>Saved Listings </h1>
       {data.user.listings.map(listing => {
         console.log(listing)
         return (
           <ListingEntryProfile price={listing.listingPrice} address={listing.listingAddress} city={listing.listingCity} state={listing.listingState} zip={listing.listingZip} image={listing.images} />
         )
       })}
-    </div>
+    </div>  
   )
 }
 
@@ -34,18 +34,17 @@ const ListingEntryProfile = (props) => {
   
  
   return (
-    <div className="listing-card listing-results-wrapper">
+    <div className="listing-card for-sale">
       <div className="image">
-        {
-          props.images !== null ? <img src={props.images} /> : <span> NO PHOTO AVAILABLE</span>
+        {props.images !== null ? <img src={props.images} /> : <span> NO PHOTO AVAILABLE</span>
         }
       </div>
       <div className="details">
         
-        <div className="price">
+        <div className="price details">
           <em>List Price: ${props.price}</em>
         </div>
-        <div className="address">
+        <div className="address details">
           {props.address}, {props.city}, {props.state} {props.zip}
         </div> 
       </div>
