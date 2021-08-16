95a27dfc3b84291e5a4535ab28e07621a3969060
Amy Smerlick <77814900+amysmerlick@users.noreply.github.com>
2021-08-15 20:56:31 -0400

    updated footer to link Github and Heroku sites

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -16,8 +16,8 @@ function Footer() {
                     <a className="footer-a" href="/Logout">Logout</a>
                 </div>
                 <div className="coding-links">
-                    <a className="coding-a after-line" href="">Heroku</a>
-                    <a className="coding-a" href="">GitHub</a>
+                    <a className="coding-a after-line" href="https://homepage-realtor-app.herokuapp.com/">Heroku</a>
+                    <a className="coding-a" href="https://github.com/johnbatista1223/realtor-app">GitHub</a>
                 </div>
             </div>
             <div className="info">
