0f7f6c0e42101b798d957c2154b408100eaa643d
Amy Smerlick <77814900+amysmerlick@users.noreply.github.com>
2021-08-16 20:40:42 -0400

    removed all console.logs from the application

--- server/app.js
+++ server/app.js
@@ -42,7 +42,5 @@ app.use(function(req, res, next) {
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -19,7 +19,6 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log('Invalid token');
     }
 
     return req;
